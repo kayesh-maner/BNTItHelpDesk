4a2d9eff910352cf922ccc59d32b728bea9e9abf
Ashok Bhambare <ashok.bhambare@bnt-soft.com>
2023-10-26 12:16:37 +0530

    Initial Commit

---
+++ apps/client/pages/admin/index.js
@@ -0,0 +1,15 @@
+export default function BlankPage() {
+  return (
+    <div className="flex flex-col">
+      <div className="flex align-middle flex-row justify-center">
+        <img className="h-12 w-auto" src="/bnt.png" alt="logo" />
+        <h1 className="text-2xl ml-2 mt-3 font-extrabold text-green-600">
+          BNT IT Helpdesk 
+        </h1>
+      </div>
+      <h1 className="font-bold text-xl text-center">
+        Welcome to BNT
+      </h1>
+    </div>
+  );
+}

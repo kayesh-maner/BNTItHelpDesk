d7d5f09b7adcbb92438c4cea826d4e98ac50c8db
Ashok Bhambare <ashok.bhambare@bnt-soft.com>
2023-11-01 11:57:39 +0530

    Count Done

--- apps/client/pages/api/v1/data/count/completed-tickets.js
+++ apps/client/pages/api/v1/data/count/completed-tickets.js
@@ -8,7 +8,13 @@ export default async function handler(req, res) {
 
   try {
     const result = await prisma.ticket.count({
-      where: { isComplete: true, userId: session.user.id },
+      where: {
+        isComplete: true,
+        OR: [
+          { userId: session.user.id },
+          { creator: session.user.id },
+        ],
+      },
     });
 
     res.status(200).json({ result });

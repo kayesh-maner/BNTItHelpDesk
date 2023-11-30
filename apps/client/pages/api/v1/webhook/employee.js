653699c78146b34c6c33aedd033bb21cd574be11
dipakjdj <kaisahemed.maner@bnt.local>
2023-11-30 14:14:17 +0530

    encryption

--- apps/client/pages/api/v1/webhook/employee.js
+++ apps/client/pages/api/v1/webhook/employee.js
@@ -1,13 +1,13 @@
 
-import { isEmpty } from "lodash";
+import { isEmpty,omit, pick } from "lodash";
 const { prisma } = require("../../../../prisma/prisma");
-
-
+import { Prisma } from '@prisma/client';
+import CryptoJS from 'crypto-js';
 
 
 const checkMandatoryFields=(reqBody) =>{
   const missingFields =[]
-  const mandatoryFields = ["email"];
+  const mandatoryFields = ["id","email"];
   // Iterate through mandatory fields
   mandatoryFields.forEach((field) => {
     if (!reqBody[field]) {
@@ -16,6 +16,15 @@ const checkMandatoryFields=(reqBody) =>{
   });
   return missingFields;
 }
+const checkSchemaFieldsvalidatBody=(body) =>{
+  const userModelData=Prisma.dmmf.datamodel.models.find(model => model.name === "User").fields
+  const userModelfield = userModelData.map((obj)=>obj.name)
+  let validParams = pick(body,userModelfield)
+  // lets not update  created at and updated_at and some important things  
+  validParams = omit(validParams,['notify_ticket_created','notify_ticket_status_changed','notify_ticket_comments','notify_ticket_assigned'])
+  return validParams
+}
+
 /**
  * @swagger
  * /api/v1/webhook/employee:
@@ -29,31 +38,40 @@ const checkMandatoryFields=(reqBody) =>{
 
 export default async function employeeWebhook(req, res) {
   try {
+
     // lets create some level of security by checking some secrte id maybe
     // will check with crypto.js same has to be working with HRM
-
     
     //lets check mandatory fields  
     const missing_filed=checkMandatoryFields(req.body)
     if(!isEmpty(missing_filed)){
       // throw error
+      res.status(500).json({ message:`mandatory Field missing ${missing_filed}` });
     }
-    console.log("\n\n\n\n req.body",req.body);
     const {
       id,
       email
     } =req.body
+
+    console.log("\n\n\n req.body",req.body)
+
+    const decrypted = await CryptoJS.AES.decrypt(req.body.encryptedData, 'NidaBandukwale');
+    console.log("\n\n\n decrypted",decrypted)
+
+    const str = await decrypted.toString(CryptoJS.enc.Utf8);
+
+    console.log("\n\n\n str",str)
     // ready steady pooo
-    const upsertBody =req.body
+    let upsertBody =req.body
+    upsertBody= checkSchemaFieldsvalidatBody(upsertBody)
     const upsertResp=await prisma.user.upsert({
       where: { id:id,email:email },
-      update: upsertBody,
-      create: upsertBody,
+      update: {...upsertBody,"updatedAt":new Date()},
+      create: {...upsertBody,"updatedAt":new Date(),"createdAt":new Date()},
     });
-    res.status(200).json(upsertResp);
+    res.status(200).json(omit(upsertResp,['password','updatedAt','createdAt']));
   } catch (error) {
-    console.log("\n\n\n\n error",error);
-    (error);
+    console.log("Something went wrong in v1/webhook/employee.js",error);
     res.status(500).json({ error });
   }
 }

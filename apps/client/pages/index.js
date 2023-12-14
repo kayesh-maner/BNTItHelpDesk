65051f8345a9d4ce60b5573f9cdfc62a168cafc5
Ashok Bhambare <ashok.bhambare@bnt-soft.com>
2023-12-14 15:33:34 +0530

    #

--- apps/client/pages/index.js
+++ apps/client/pages/index.js
@@ -261,7 +261,7 @@ export default function Home() {
                         scope='col'
                         className='py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0'
                       >
-                        #
+                        SR No
                       </th>
                       <th
                         scope='col'

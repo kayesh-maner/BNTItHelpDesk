f8ab27dace9bc57cd34f515d1d504c791dc27a58
Ashok Bhambare <ashok.bhambare@bnt-soft.com>
2024-01-09 16:40:27 +0530

    test

--- apps/client/lib/nodemailer/ticket/comment.js
+++ apps/client/lib/nodemailer/ticket/comment.js
@@ -1,30 +1,37 @@
-import nodeMailer from "nodemailer";
-import { prisma } from "../../../prisma/prisma";
-import { commentTicketTemplate } from '../../../templates/commentTicket';
-
+import nodeMailer from 'nodemailer'
+import { prisma } from '../../../prisma/prisma'
+import { commentTicketTemplate } from '../../../templates/commentTicket'
 
 export async function sendTicketComment(session, comment, ticketData) {
-  let mail;
+  try {
+    let mail
 
-  const emails = await prisma.email.findMany();
-  if (emails.length > 0) {
-      const emailConfig = emails[0];
-      const { host, port, secure, user, pass } = emailConfig;
+    const emails = await prisma.email.findMany()
+    if (emails.length > 0) {
+      const emailConfig = emails[0]
+      const { host, port, secure, user, pass } = emailConfig
       mail = nodeMailer.createTransport({
         host: host,
-        port: port, 
-        secure: secure, 
+        port: port,
+        secure: secure,
         auth: {
           user: user,
           pass: pass,
         },
-      });
-    
-    const mailData = {
-       from: 'noreply@bnt-soft.com', // sender address
-       to: [ticketData.email, process.env.ADMIN_EMAIL],
-       cc: [ticketData.cc],
+      })
+
+      const mailData = {
+        from: 'noreply@bnt-soft.com', // sender address
+        to: [ticketData.email, process.env.ADMIN_EMAIL],
+        cc: [ticketData.cc],
+      }
+      let test = await mail.sendMail({
+        ...mailData,
+        ...commentTicketTemplate(session, comment, ticketData),
+      })
+      console.log('comment mail var >>>', test)
     }
-     await mail.sendMail({ ...mailData, ...commentTicketTemplate(session, comment, ticketData) });   
+  } catch (err) {
+    console.log('Comment Catch Error >>>', err)
   }
 }
--- apps/client/lib/nodemailer/ticket/create.js
+++ apps/client/lib/nodemailer/ticket/create.js
@@ -1,36 +1,35 @@
-import nodeMailer from "nodemailer";
-import { prisma } from "../../../prisma/prisma";
-import { createTicketTemp } from "../../../templates/createTicket";
-
+import nodeMailer from 'nodemailer'
+import { prisma } from '../../../prisma/prisma'
+import { createTicketTemp } from '../../../templates/createTicket'
 
 export async function sendTicketCreate(ticket, session) {
   try {
-    let mail;
-    const emails = await prisma.email.findMany();
+    let mail
+    const emails = await prisma.email.findMany()
 
     if (emails.length > 0) {
       // if (process.env.NODE_ENV === "development") {
-        const emailConfig = emails[0];
-        const { host, port, secure, user, pass } = emailConfig;
-        mail = nodeMailer.createTransport({
-          host: host,
-          port: port, 
-          secure: secure, 
-          auth: {
-            user: user,
-            pass: pass,
-          },
-        });
-      // } 
-        await mail.sendMail(
-        { from: 'noreply@bnt-soft.com', // sender address
-         to: [ticket.email, process.env.ADMIN_EMAIL],
-         cc: ticket.cc,
-         ...createTicketTemp(ticket, session)
-        });
-
+      const emailConfig = emails[0]
+      const { host, port, secure, user, pass } = emailConfig
+      mail = nodeMailer.createTransport({
+        host: host,
+        port: port,
+        secure: secure,
+        auth: {
+          user: user,
+          pass: pass,
+        },
+      })
+      // }
+      await mail.sendMail({
+        from: 'noreply@bnt-soft.com', // sender address
+        to: [ticket.email, process.env.ADMIN_EMAIL],
+        cc: ticket.cc,
+        ...createTicketTemp(ticket, session),
+      })
     }
   } catch (error) {
+    console.log('Error >>>', error)
     return error
   }
 }
